2f863451944078c1510c7f29675434168d327235
Nguyễn Minh Nhật <116283788+nmnhatUIT03@users.noreply.github.com>
2023-11-29 08:57:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ st.title('Data Visualization')
 
 st.file_uploader('Choose a csv file', type=(['.csv'])
 
-if data_file is not None:
+if data_file is not None
   df = pd_read_csv(data_file)
   st.dataframe(df)
